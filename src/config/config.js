46e32c3439c678dabbb6a34d31d932b6f853002b
EduardoVargasZumaeta <u22234544@utp.edu.pe>
2026-01-18 20:55:17 -0500

    feat: add initial configuration file with environment variable loading

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
-const dotenv = require("dotenv");
+import dotenv from "dotenv";
 dotenv.config();
 
-module.exports = {
+export default {
   GROUP_ID: process.env.GROUP_ID,
   DB_PATH: process.env.DB_PATH || "./alumnos.json",
   TIMEZONE: process.env.TIMEZONE || "America/Lima",
-  CRON_SCHEDULE: process.env.CRON_SCHEDULE || "30 8 * * *",
+  CRON_SCHEDULE: process.env.CRON_SCHEDULE || "* 21 * * *", // Cada día a las 21:00
 };
